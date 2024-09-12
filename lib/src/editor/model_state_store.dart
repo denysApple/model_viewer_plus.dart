5cbcfe5f9e17ad09ce272aabeee79944d0e23e6c
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-09-13 01:09:26 +0300

    Revert "logging through js channels"
    This reverts commit 234ac44f97907ab8281c555dd19acdc74bb4688f.

--- lib/src/editor/model_state_store.dart
+++ lib/src/editor/model_state_store.dart
@@ -5,13 +5,13 @@ import 'package:rxdart/subjects.dart';
 import 'model_state.dart';
 
 class ModelStateStore {
-  final _stateController = BehaviorSubject<ModelState>();
+  final _stateController = BehaviorSubject<ModelState>.seeded(ModelState());
   Stream<ModelState> get state => _stateController.stream;
 
   void updateState(
     ModelState Function(ModelState) producer,
   ) {
-    final currentState = _stateController.valueOrNull ?? ModelState();
+    final currentState = _stateController.value;
     final newState = producer(currentState);
     _stateController.sink.add(newState);
   }
