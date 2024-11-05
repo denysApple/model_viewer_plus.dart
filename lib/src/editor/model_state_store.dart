8a5b29e634c0ce30f17e64ffb6444c4416c558d7
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2024-11-05 16:40:04 +0500

    - fixed the bug in the object re-creation

--- lib/src/editor/model_state_store.dart
+++ lib/src/editor/model_state_store.dart
@@ -7,22 +7,34 @@ import 'model_state.dart';
 class ModelStateStore {
   final _stateController = BehaviorSubject<ModelState>();
   final _exportClicks = PublishSubject<bool>();
+
   Stream<ModelState> get state => _stateController.stream;
+
   Stream<bool> get exportClicks => _exportClicks.stream;
 
-  void updateState(
-    ModelState Function(ModelState) producer,
-  ) {
+  ModelStateStore() {
+    print('ModelStateStore: Instance created');
+  }
+
+  void updateState(ModelState Function(ModelState) producer,) {
     final currentState = _stateController.valueOrNull ?? ModelState();
     final newState = producer(currentState);
-    _stateController.sink.add(newState);
+    try {
+      _stateController.sink.add(newState);
+      print('ModelStateStore: State updated to $newState');
+    } catch (e) {
+      print('ModelStateStore: Error updating state: $e');
+    }
   }
 
   void dispose() {
     _stateController.close();
+    _exportClicks.close();
+    print('ModelStateStore: Instance disposed');
   }
 
   void export() {
     _exportClicks.sink.add(true);
   }
+
 }
